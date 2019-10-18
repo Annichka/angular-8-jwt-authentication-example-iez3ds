5923506429e022e0e491ad4dac1d2710b50f8219
Annichka <aakhv13@freeuni.edu.ge>
2019-10-18 16:38:46 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-jwt-authentication-example-iez3ds
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8-jwt-authentication-example-iez3ds)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { UserService, AuthenticationService } from '../_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    users: User[];
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+        this.loading = true;
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+            this.users = users;
+        });
+    }
+}
